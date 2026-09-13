f55cb8246ac465b1856821bec5fbf6cb070cc69c
Shreeram Haridas <shreeramkh100@gmail.com>
2026-09-13 18:54:02 +0530

    Basic Express and CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+// Basic Configurations
+app.use(express.json({limit:'16kb'}));//JSON body parsing
+app.use(express.urlencoded({extended:true,limit:'16kb'}));//Form body parsing
+app.use(express.static("public"));//Static file serving
+
+//  CORS Configurations
+app.use(cors({
+    origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders:["Content-Type","Authorization"]
+}))
 
 app.get('/',(req,res)=>{
     res.end("Tu kya kar raha hai mere bhai")
