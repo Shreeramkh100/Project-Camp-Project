5d3a0f87d327e26ce3c55e62ee419c76d828adee
Shreeram Haridas <shreeramkh100@gmail.com>
2026-09-13 12:00:45 +0530

    Setup of Express Server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log('Name:',process.env.user)
-console.log("Backend Project");
+import express from "express";
+const PORT=process.env.PORT||1002;
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.end("Tu kya kar raha hai mere bhai")
+})
+
+app.listen(PORT,()=>{
+     console.log(`Example app listening on port ${PORT}`)
+})
